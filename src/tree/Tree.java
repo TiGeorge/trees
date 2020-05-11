668f6268d504d316daf9b7e26262acc62a7b2ebe
TiGeorge <georgetikhomirov@mail.ru>
2020-05-11 16:22:09 +0300

    commit

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -35,8 +35,77 @@ public class Tree {
         }
     }
 
-    public void delete(int id) {
+    public void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.leftChild);
+            System.out.print(node.id + " ");
+            inOrder(node.rightChild);
+        }
+    }
 
+    public Node minimum() {
+        Node current = root;
+        Node result = null;
+        while (current != null) {
+            result = current;
+            current = current.leftChild;
+        }
+        return result;
+    }
+
+    private Node getSuccessor(Node node) {
+        Node parent = node;
+        Node successor = node.rightChild;
+        while (successor.leftChild != null) {
+            parent = successor;
+            successor = successor.leftChild;
+        }
+        if (node.rightChild != successor) {
+            parent.leftChild = successor.rightChild;
+            successor.rightChild = node.rightChild;
+        } else {
+            successor.leftChild = node.leftChild;
+        }
+        return successor;
+    }
+
+    public boolean delete(int id) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChaild = false;
+        while (current.id != id) {
+            parent = current;
+            if (id < current.id) {
+                current = current.leftChild;
+                isLeftChaild = true;
+            } else {
+                current = current.rightChild;
+                isLeftChaild = false;
+            }
+            if (current == null) return false;
+        }
+        if (current.leftChild == null && current.rightChild == null) {
+            if(current == root) root = null;
+            else if(isLeftChaild) parent.leftChild = null;
+            else parent.rightChild = null;
+        }
+
+        if (current.rightChild == null) {
+            if(current == root) root = current.leftChild;
+            else if(isLeftChaild) parent.leftChild = current.leftChild;
+            else parent.rightChild = current.leftChild;
+        } else if (current.leftChild == null) {
+            if (current == root) root = current.rightChild;
+            else if (isLeftChaild) parent.leftChild = current.rightChild;
+            else parent.rightChild = current.rightChild;
+        } else {
+            Node successor = getSuccessor(current);
+
+            if (current == root) root = successor;
+            else if (isLeftChaild) parent.leftChild = successor;
+            else parent.rightChild = successor;
+        }
+        return true;
     }
 }
 /////////////////////////////////////////////////////////////////////
--- src/tree/TreeApp.java
+++ src/tree/TreeApp.java
@@ -5,7 +5,10 @@ public class TreeApp {
         Tree theTree = new Tree();
         theTree.insert(50, 1.5);
         theTree.insert(25, 1.7);
-        theTree.insert(75, 1.9);
+        theTree.insert(75, 1.7);
+        theTree.insert(12, 1.9);
+        theTree.insert(40, 1.9);
+        theTree.insert(100, 1.9);
 
         Node found = theTree.find(25);
         if (found != null) {
@@ -14,5 +17,11 @@ public class TreeApp {
             System.out.println("Could not find node with key 25");
         }
 
+        theTree.inOrder(theTree.root);
+        System.out.println();
+
+        System.out.println(theTree.delete(75));;
+
+        theTree.inOrder(theTree.root);
     }
 }
